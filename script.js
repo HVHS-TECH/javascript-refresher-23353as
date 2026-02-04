5b08ac8d2474ad306b5629ff6da95922ba205b1a
Aaron Shaji <23353as@hvhs.school.nz>
2026-02-04 14:57:35 +1300

    Added a button that gets the users name and welcomes them

--- script.js
+++ script.js
@@ -3,3 +3,9 @@
 function start() {
  document.getElementById("welcomeMessage").innerHTML = "Hello World";
 }
+
+function useInput() {
+  const name = document.getElementById("nameInput").value;
+  document.getElementById("welcomeMessage").innerHTML =
+    "Hello " + name;
+}
